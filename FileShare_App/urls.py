3df8724ccbf8d5eff72b8ba4fcd1e050b5674968
EspaMC16 <markchristianespanol0516@gmail.com>
2025-06-01 18:44:43 +0800

    Fix storagae

--- FileShare_App/urls.py
+++ FileShare_App/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from . import views
 
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', views.home, name='home'),  # This makes it the default/home route
     path('upload/', views.upload_file, name='upload_file'),
     path('success/', views.upload_success, name='upload_success'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
