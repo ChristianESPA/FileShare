b6350e697a4f4252fa617e1de536caa2c755b2b6
EspaMC16 <markchristianespanol0516@gmail.com>
2025-06-01 19:28:35 +0800

    allowed all host

--- FileShare/settings.py
+++ FileShare/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)od894oa(dv41zva47io(b4p+$=)aeis5z$i4&**&99*r(x5r(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
